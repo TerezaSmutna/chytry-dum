73c25ac3a18c2aca11b8fbbccac7453e53b7510b
Tereza Smutna <terka.kop@seznam.cz>
2023-03-06 15:14:36 +0100

    Komponenty Header a Dashboard

---
+++ src/Dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Dashboard = ({ data }) => {
+  return (
+    <main className="dashboard">
+    </main>
+  );
+};
+
+
+export default Dashboard;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Header from './Header';
+import Dashboard from './Dashboard';
+import { smartHomeData } from './smartHomeData.js';
 
 const App = () => (
   <>
-
+    <div className="container">
+      {
+        <Header
+          title = 'Chytrý dům'
+        />
+      }
+      {
+        <Dashboard
+          data = {smartHomeData}
+        />
+      }
+    </div>
   </>
 );
 
